95901f638275bbe547d3a06575e195e00db64f7d
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-08-17 20:24:45 -0400

    todo list

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -6,27 +6,46 @@ using BreadmakerReport.Models;
 
 namespace BreadmakerReport
 {
+   
     class Program
-    {
+    
+{
         static string dbfile = @".\data\breadmakers.db";
         static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
 
         static void Main(string[] args)
-        {
+       
+        
+    {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+            var TempList = BreadmakerDb.Breadmakers
+                .Select(item => new{
+                    desc = item.title,
+                    rev = item.Reviews.Count(),
+                    avg = (Double)BreadmakerDb.Reviews
+                        .Where(i =>i.BreadmakerId == item.BreadmakerId)
+                        .Select(i => i.stars).Sum() / item.Reviews.Count(), })
                 .ToList();
 
-            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
-            for (var j = 0; j < 3; j++)
+            var BMList = TempList
+                .Select(item => new{
+                    description = item.desc,
+                    review = item.rev,
+                    average = item.avg,
+                    adjust = ratingAdjustmentService.Adjust(item.avg, item.rev) })
+                .OrderByDescending(i => i.adjust)
+                .ToList();
+
+
+            Console.WriteLine("[#] \t Reviews \t Average \t Adjust \t Description");
+            for (var i = 0; i < 3; i++)
+          
+            
             {
-                var i = BMList[j];
-                // TODO: add output
-                // Console.WriteLine( ... );
+                var item = BMList[i];
+                Console.WriteLine($"[{i + 1}] \t {item.review,7} \t {Math.Round(item.average, 2),-7} \t {Math.Round(item.adjust, 2),-6} \t  {item.description}");
             }
-        }
-    }
+}
+}
 }
